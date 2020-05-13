ea6c955f2a6f19300539ddc62a80ee0979c33e65
Alex Markov <49829241+CNFault@users.noreply.github.com>
2020-05-13 20:01:44 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,88 +1,99 @@
-class Student {
-    constructor({ fullName, university, course, photoUrl, birthYear }) {
-        this.fullName = fullName;
-        this.university = university;
+class Student extends Person {
+    constructor(name, surname, photoUrl, birthYear, course, group) {
+        super(name, surname, photoUrl, birthYear, course, group);
+        this.type = 'Студент';
         this.course = course;
-        this.photoUrl = photoUrl;
-        this.birthYear = birthYear
+        this.group = group
+    }
+    get status() {
+        return `${this.type}, ${this.course} курс, группа ${this.group}`
+    }
+}
+
+class Teacher extends Person {
+    constructor(name, surname, photoUrl, birthYear, expirience, position) {
+        super(name, surname, photoUrl, birthYear);
+        this.type = 'Преподаватель';
+        this.expirience = expirience;
+        this.position = position
     }
 
-    get age() {
-        return new Date().getFullYear() - this.birthYear; //Для доступа к компонентам даты-времени объекта Date используется  метод getFullYear() - Получить год (из 4 цифр)
+    get status() {
+        return `${this.type}`
     }
-    get education() {
-        return `${this.university} ${this.course}`;
-    }
-    appendStudentBlock() {
+
+    render() {
         const div = document.createElement('div');
-        div.className = 'student';
+        div.className = 'teacher';
         div.innerHTML =
-            `<img src=".${this.photoUrl}" alt="Аватар 1" title="Аватар 1" /><p title="Фамилия Имя">${this.fullName}</p><span title="ВУЗ Курс">${this.education} курс</span>`;
-        document.body.appendChild(div);
-
+            `<img src=".${this.photoUrl}" alt="Аватар ${this.id}" title="Аватар ${this.id}" /><p title="Фамилия Имя">${this.fullName}</p><span title="ВУЗ Курс"> ${this.position} </span>`;
         div.addEventListener('click', () => {
             openCard(this);
         });
+        return div;
     }
-
 }
 
+class PersonFactory {
+    create(type, ...args) {
+        switch (type) {
+            case 'Студент':
+                return new Student(...args);
+            case 'Преподаватель':
+                return new Teacher(...args);
+            default:
+                return new Person(...args);
+        }
+    }
+}
 
+class School {
+    constructor(mountElement) {
+        this.mountElement = mountElement;
+        this.persons = [];
+        this.pf = new PersonFactory;
+    }
+    enroll(type, ...args) {
+        const person = this.pf.create(type, ...args);
+        this.persons.push(person);
+    }
+    dismiss(person) {
+        const index = this.persons.indexOf(person);
+        this.persons.splice(index, 1);
+    }
+    search(name) {
+        return this.persons.filter(person => person.name === name);
+    }
+    mount() {
+        this.persons.forEach((person) => {
+            person.mount(this.mountElement);
+        });
+    }
+}
 
-const studentArr = [
-    {
-        fullName: 'Алексей Шипелкин',
-        university: 'УГАТУ',
-        course: 2,
-        photoUrl: '/img/ava01.jpg',
-        birthYear: 2000
-    },
-    {
-        fullName: 'Мария Фёдорова',
-        university: 'НГТУ',
-        course: 3,
-        photoUrl: '/img/ava02.jpg',
-        birthYear: 2001
-    },
-    {
-        fullName: 'Кристина Сидорова',
-        university: 'НГАСУ',
-        course: 1,
-        photoUrl: '/img/ava03.jpg',
-        birthYear: 1998
-    },
-    {
-        fullName: 'Андрей Молотов',
-        university: 'НГАХА',
-        course: 5,
-        photoUrl: '/img/ava04.jpg',
-        birthYear: 1990
-    },
-    {
-        fullName: 'Ирина Васильева',
-        university: 'НГУ',
-        course: 4,
-        photoUrl: '/img/ava05.jpg',
-        birthYear: 1992
-    },
-    {
-        fullName: 'Николай Дроздов',
-        university: 'ЮУРГУ',
-        course: 2,
-        photoUrl: '/img/ava06.jpg',
-        birthYear: 2003
-    },
+const school = new School(document.querySelector('.element'));
+
+let personList = [
+    ['Студент', 'Алексей', 'Шипелкин', '/img/ava01.jpg', 2000, 4, 'ЭММ-13'],
+    ['Студент', 'Мария', 'Фёдорова', '/img/ava02.jpg', 2001, 3, 'РФ-13'],
+    ['Студент', 'Кристина', 'Сидорова', '/img/ava03.jpg', 1998, 1, 'ФЭН-3'],
+    ['Преподаватель', 'Андрей', 'Молотов', '/img/ava04.jpg', 1990, 5, 'Профессор'],
+    ['Преподаватель', 'Ирина', 'Васильева', '/img/ava05.jpg', 1986, 7, 'Доцент'],
+    ['Студент', 'Николай', 'Дроздов', '/img/ava06.jpg', 2003, 4, 'ОР']
 ];
 
-studentArr.forEach((item) => {
-    const student = new Student(item);
-    student.appendStudentBlock();
+
+personList.forEach((data) => {
+    school.enroll(...data);
+    /*Person.appendStudentBlock();*/
 });
+
+school.mount();
 
 const openCard = (student) => {
     const popup = document.querySelector('.popup');
     const content = document.querySelector('.content');
-    content.innerHTML = `<div><img class="minImg" src=".${student.photoUrl}" alt="Аватар" title="Аватар" /></div><p title="Фамилия Имя">${student.fullName} </p><span title="Возраст">${student.age} лет</span>`;
+    content.innerHTML = `<div><img class="minImg" src=".${student.photoUrl}" alt="Аватар" title="Аватар" /></div><p title="Фамилия Имя">${student.status} </p><span title="Возраст">${student.age} лет</span>`;
     popup.style.display = 'block';
 
     const close = document.querySelector('.close');
@@ -91,3 +102,24 @@ const openCard = (student) => {
     });
 }
 
+school.dismiss(['Преподаватель', 'Андрей', 'Молотов', '/img/ava04.jpg', 1990, 5, 'Профессор']);
+console.log(personList);
+
+/*
+function text() {
+    let txt;
+    count = this.teachingExperience;
+    if (count >= 5 && count <= 20) {
+        txt = 'лет';
+    } else {
+        count = this.teachingExperience % 10;
+
+        if (count == 1) {
+            txt = 'год';
+        } else if (count >= 2 && count <= 4) {
+            txt = 'года';
+        } else {
+            txt = 'лет';
+        }
+    }
+    return txt;*/
